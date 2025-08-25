e36e4828495adbdfc91377d88c846d9e724dd103
Parikshit Bansal <parikshitb505@gmail.com>
2025-08-26 02:08:13 +0545

    Add files via upload

---
+++ Methods.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    
+    int num1, num2;
+    int sum, difference, product;
+    float quotient;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    sum = num1 + num2;
+    diffrence = num1 - num2;
+    product = num1 * nun2;
+    
+    if (num2 !=0) {
+        quotient = (float)num1 /num2;
+        printf("\nQuotient: %.2f\n",quotient);
+    }else {     
+        printf("\nDivision not possible (deniominator is 0).\n");
+    
+    }
+
+    printf("\nSum: %d", sum);
+    printf("\nDifference: %d", difference);
+    printf("\nProduct; %d", product);
+
+    return 0;
+}
