dc095c6bfd7a73435eb838f8780d110edd3b2116
Parikshit Bansal <parikshitb505@gmail.com>
2025-08-29 11:03:20 +0545

    Add files via upload

---
+++ DAY3ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+
+    int a, b, ace;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    ace = a;
+    a = b;
+    b = ace;
+
+    printf("After swapping: \n");
+    printf("First number = %d\n", a);
+    printf("Second number = %d\n", b);
+
+    return 0;
+}
