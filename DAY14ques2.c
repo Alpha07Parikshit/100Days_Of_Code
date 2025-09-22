99a08821ced69f62f449199ad99620f00ac613b7
Parikshit Bansal <parikshitb505@gmail.com>
2025-09-23 03:24:19 +0545

    Add files via upload

---
+++ DAY14ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long product = 1; 
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+
+    if (n < 2) {
+        printf("No even numbers in the range.\n");
+    } else {
+        printf("Product of even numbers from 1 to %d is %llu\n", n, product);
+    }
+    return 0;
+}
